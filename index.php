881d538f7e50c6c6fb2e8f62ecc9ce3cc2921eb6
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-10 09:44:57 +0700

    a
    a

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="style1.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
 	<!-- jQuery library -->
@@ -17,21 +17,19 @@
 <body>
 	<div style="background-image: url('img/backgroundHome.jpg'); background-size: 100%">
 		<div class="container">
+			<div class="row">
+				<div class="<col-sm-3>">
+					<img src="img/Logo.png" alt="" style="width: 40%; height: auto;">
+				</div>
+			</div>
+		</div>
 
+
+
+		<div class="container">
 			<div class="row" style=" border-radius: 30px; border-style: solid;">
-				<div>
-					<ul>
-						<li class="menu"><a href="#home">Home</a></li>
-						<li>-</li>
-						<li class="menu"><a href="#news">News</a></li>
-						<li>-</li>
-						<li class="menu"><a href="#contact">Contact</a></li>
-						<li>-</li>
-						<li class="menu"><a href="#about">About</a></li>
-					</ul>
-				</div>			
-				<div class="col-sm-2" style="background-color: green; border-top-left-radius:20px; padding: 10px; border-bottom-left-radius: 20px; margin-top: 2%">
-					<h1>Dịch vụ</h1>
+				<div class="col-sm-2" style="background-color: green; border-top-left-radius:20px; padding: 10px; border-bottom-left-radius: 20px; overflow:auto; font-size: 90%">
+					<h1 style="margin-top: 20%;">Dịch vụ</h1>
 					<a href="" title="">Tư vấn xây dựng</a><br>
 					<a href="" title="">Tư vấn đầu tư</a><br>
 					<a href="" title="">Quy hoạch đô thị</a><br>
@@ -48,8 +46,49 @@
 					<a href="" title="">Nhà máy công nghiệp</a><br>
 					<a href="" title="">Công trình công cộng</a><br>
 					<a href="" title="">Nhà mẫu, Biệt thự</a><br>
+					<img src="img/hotline.PNG" alt="" style="width: 170px; height: 50px">
+					<div style="margin-top: 5%" >
+						<a href="https://www.facebook.com/" target="new"><img src="img/facebook.PNG" style="width:35px;height:35px;"></a>
+						<a href="https://mail.google.com/" target="new"><img src="img/mail.PNG" style="width:35px;height:35px;"></a>
+						<a href="https://www.skype.com/en/" target="new"><img src="img/skype.PNG" style="width:35px;height:35px;"></a>
+						<a href="https://vn.yahoo.com/" target="new"><img src="img/yahoo.jpg" style="width:35px;height:35px;"></a>	
+					</div>
 				</div>
-				<div class="col-sm-10" style="background-color: green; border-top-right-radius: 20px; border-bottom-right-radius: 20px; margin-top: 2%">
+			
+				<div class="col-sm-10" style="background-color: green; border-top-right-radius: 20px; border-bottom-right-radius: 20px;">
+					<div>
+						<div class="topnav" id="myTopnav">
+							<a href="#home" class="active">Home</a>
+							<a href="#news">News</a>
+							<a href="#contact">Contact</a>
+							<div class="dropdown">
+								<button class="dropbtn">Dropdown 
+									<i class="fa fa-caret-down"></i>
+								</button>
+								<div class="dropdown-content">
+									<a href="#">Link 1</a>
+									<a href="#">Link 2</a>
+									<a href="#">Link 3</a>
+								</div>
+							</div> 
+							<a href="#about">About</a>
+							<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+						</div>
+
+
+						<script>
+							function myFunction() {
+								var x = document.getElementById("myTopnav");
+								if (x.className === "topnav") {
+									x.className += " responsive";
+								} else {
+									x.className = "topnav";
+								}
+							}
+						</script>
+
+
+					</div>	
 					<div style="margin-top: 10px; margin-bottom: 10px;">
 						<img src="img/2.jpg" class="mx-auto d-block" style="width:100%; height: auto;"> 
 					</div>
@@ -57,8 +96,28 @@
 				</div>
 
 
+
 			</div>
+			<!-- phan lien he  -->
+			<div id="footer">
+			<div class="endpage">
+			<table border="0" width="100%" cellspacing="0" cellpadding="0">
+				<tbody>
+				<tr>
+				<td align="left" valign="top">
+				<p style="font-size:12px">
+	   				     <strong>Copyright © 2011: </strong><strong>Công Ty TNHH Phát Triển Dự Án Song Nam </strong>- Hotline : <strong>0769 861 168</strong><br>
+				   		 <strong>Trụ sở chính: </strong>98 Trần Quang Khải, P. Tân Định, Quận 1, TP HCM - Tel: + (84.28) 3848 4995 - Fax: + (84.28) 35 265 269<br>
+	     			     <strong>Bình Dương: </strong>401/36 Lê Hồng Phong, P. Phú Hòa, TP. Thủ Dầu Một, Bình Dương - Tel / Fax: + (84 650) 385 6689<br>
+						 <strong>Song Nam USA Corporation:</strong> 26521 Lilac Hill Dr, Escondido, CA 92026, United States<br>
+	 			</p>
+	  	        </td>
+				</tr>
+			</tbody></table>
+
 		</div>
+
 	</div>
+	
 </body>
 </html>
