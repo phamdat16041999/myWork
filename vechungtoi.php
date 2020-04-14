2ce026f97a0f7ba48ec8c8f2548f6a07b030351e
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-14 11:54:01 +0700

    Update vechungtoi.php

--- vechungtoi.php
+++ vechungtoi.php
@@ -41,78 +41,62 @@
 			<div class="col-lg-8 col-md-12" id="boderright">
 				<div class="row">
 					<div class="col-lg-12 col-md-12">						
-						<div class="topnav" id="myTopnav">
-							<a href="index.php" class="active">Trang chủ</a>
-							<a href="vechungtoi.php">Về chúng tôi</a>
-							<a href="lienhe.php">Liên hệ</a>
-							<a href="#about">About</a>
-							<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+						<?php 
+						include 'menuTop.php';
+						?>
+						<div class="col-lg-6 col-md-12" style="margin-top: 15px">
+							<a href="thungo.php" title="">
+								<div class="project">
+									<img src="img/thungo.png" alt="Avatar" class="imageProject" style="background-color: white;">
+									<div class="overlay">
+										<div class="textproject">Thư ngỏ</div>
+									</div>
+								</div>
+							</a>
 						</div>
-					</div>
+						<div class="col-lg-6 col-md-12" style="margin-top: 15px">
+							<a href="gioithieu.php" title="">
+								<div class="project">
+									<img src="img/gioithieu.png" alt="Avatar" class="imageProject">
+									<div class="overlay">
+										<div class="textproject">Giới thiệu công ty</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-6 col-md-12" style="margin-top: 15px">
+							<a href="tamnhinsumenh.php" title="">
+								<div class="project">
+									<img src="img/tamnhinsumenh.png" alt="Avatar" class="imageProject">
+									<div class="overlay">
+										<div class="textproject">Tầm nhìn, sứ mệnh</div>
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-6 col-md-12" style="margin-top: 15px;">
+							<a href="sodocongty.php" title="">
+								<div class="project">
+									<img src="img/iconsodocongty.png" alt="Avatar" class="imageProject" style="background-color: white;">
+									<div class="overlay">
+										<div class="textproject">Sơ đồ tổ chức công ty</div>
+									</div>
+								</div>
+							</a>
+						</div>
 
-					<script>
-						function myFunction() {
-							var x = document.getElementById("myTopnav");
-							if (x.className === "topnav") {
-								x.className += " responsive";
-							} else {
-								x.className = "topnav";
-							}
-						}
-					</script>
-					<div class="col-lg-6 col-md-12" style="margin-top: 15px">
-						<a href="thungo.php" title="">
-							<div class="project">
-								<img src="img/thungo.png" alt="Avatar" class="imageProject" style="background-color: white;">
-								<div class="overlay">
-									<div class="textproject">Thư ngỏ</div>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-6 col-md-12" style="margin-top: 15px">
-						<a href="gioithieu.php" title="">
-							<div class="project">
-								<img src="img/gioithieu.png" alt="Avatar" class="imageProject">
-								<div class="overlay">
-									<div class="textproject">Giới thiệu công ty</div>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-6 col-md-12" style="margin-top: 15px">
-						<a href="tamnhinsumenh.php" title="">
-							<div class="project">
-								<img src="img/tamnhinsumenh.png" alt="Avatar" class="imageProject">
-								<div class="overlay">
-									<div class="textproject">Tầm nhìn, sứ mệnh</div>
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-6 col-md-12" style="margin-top: 15px;">
-						<a href="sodocongty.php" title="">
-							<div class="project">
-								<img src="img/iconsodocongty.png" alt="Avatar" class="imageProject" style="background-color: white;">
-								<div class="overlay">
-									<div class="textproject">Sơ đồ tổ chức công ty</div>
-								</div>
-							</div>
-						</a>
-					</div>
-					
 
-				</div>	
+					</div>	
+
+				</div>
+
+
 
 			</div>
-
-
-
-		</div>
-		<!-- phan lien he  -->
-	<?php 
-	include 'enpage.php';
-	?>
+			<!-- phan lien he  -->
+			<?php 
+			include 'enpage.php';
+			?>
 
 		</body>
 		</html>
