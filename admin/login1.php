63c468a13761fa6d8db20b5bbbc31deb454ac0fc
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-15 16:52:41 +0700

    Create login1.php

---
+++ admin/login1.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<style type="text/css" media="screen">
+		.parent {
+			background-image: url('images/bg-header.jpg');
+			background-size: cover;
+			background-position: center;
+			width: 100%;
+			height: 100vh;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+
+		form {
+			display: inline-block;
+			background: #b4e9fb;
+			padding: 30px;
+			border-radius: 10px;
+			text-align: center;
+		}
+
+		form input[type="text"] {
+			width: 250px;
+			border-radius: 5px;
+			outline: 0;
+			border: 0;
+			height: 35px;
+			margin: 10px 3px 20px 3px;
+		}
+
+		input[type="submit"] {
+			outline: 0;
+			border: 0;
+			font-weight: 700;
+			color: white;
+			background: #049bcc;
+			padding: 5px 20px;
+			border-radius: 10px;
+			font-size: 20px;
+			width: 100px;
+			display: inline-block;
+		}
+		input[type="submit"]:hover{
+			opacity: .8;
+			transition: all .4s ease-in-out;
+		}
+	</style>
+</head>
+<body>
+
+	<?php include 'connect.php' ?>
+	<div class="parent">
+		
+
+		<form action="" method="GET">
+			<input type="text" name="UserName" value="User name">
+			<input type="text" name="PassWord" value="Password">
+			<div><input type="submit" value="Login" name="Login"></div>
+			<?php 
+			if(isset($_GET['UserName'])&&isset($_GET['PassWord']))
+			{
+				$a=$_GET['UserName'];
+				$b=$_GET['PassWord'];
+				$query = "SELECT username, password FROM account WHERE username = '" . $a . "' and password = '". $b ."'" ;
+				$stmt = $pdo->prepare($query);
+				$stmt->setFetchMode(PDO::FETCH_ASSOC);
+				$stmt->execute();
+				$resultSet = $stmt->fetchAll();
+
+				$c = $resultSet[0]["customerid"];
+	
+
+				if ((count($resultSet)>0))
+				{
+
+					header("Location: ./admin.php");
+					
+				}
+				else
+				{
+					echo 'aaaaaaa';
+				}
+			}
+			?>
+
+		</form>
+	</div>
+
+
+</body>
+</html>
