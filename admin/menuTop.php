abc3294589e53cd97156f873f53498396aebe400
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-15 15:42:36 +0700

    a

---
+++ admin/menuTop.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+		<div class="topnav" id="myTopnav">
+			<a href="admin/admin.php" class="active">Trang chủ</a>
+			<a href="myWork/index.php.php">Đăng xuất</a>
+		</div>
+
+
+		<script>
+			function myFunction() {
+				var x = document.getElementById("myTopnav");
+				if (x.className === "topnav") {
+					x.className += " responsive";
+				} else {
+					x.className = "topnav";
+				}
+			}
+		</script>
+</body>
+</html>
