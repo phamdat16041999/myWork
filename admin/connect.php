4a730c11a17834ac1d151563bd5c322079cdfd03
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-15 16:54:44 +0700

    Create connect.php

---
+++ admin/connect.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<style type="text/css" media="screen">
+	.img{
+		max-width: 100%;
+	    height: auto;
+	    display: block;
+	}
+	.container{
+		max-width: 1150px;
+	}
+
+</style>
+</head>
+<body>
+<?php 
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+ ?>
+</body>
+</html>
