d198bc743c0e86c1c1bbe8b4f57da628c7e108ee
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-16 17:12:33 +0700

    Update duantrienkhai.php

--- admin/duantrienkhai.php
+++ admin/duantrienkhai.php
@@ -89,8 +89,8 @@
 											<td><input type="text" name="diachi" style="width: 100%" value="<?=$resultSet[$i]["diachi"]?>"></td>
 											<td><input type="text" name="tenduan" style="width: 100%" value="<?=$resultSet[$i]["tenduan"]?>"></td>
 											<td><input type="text" name="duongdananh" style="width: 100%" value="<?=$resultSet[$i]["duongdananh"]?>"></td>
-											<td><a href="updateduantrienkhai.php?username=<?=$username?>&password=<?=$password?>&id=<?=$resultSet[$i]["diachi"]?>" title="">delete</a></td>
-											<td><a href="updateduantrienkhai.php?username=<?=$username?>&password=<?=$password?>&id=<?=$resultSet[$i]["diachi"]?>" title="">update</a></td>
+											<td><a href="updateduantrienkhai.php?username=<?=$username?>&password=<?=$password?>&id=<?=$resultSet[$i]["id"]?>" title="">delete</a></td>
+											<td><a href="updateduantrienkhai.php?username=<?=$username?>&password=<?=$password?>&id=<?=$resultSet[$i]["id"]?>" title="">update</a></td>
 										</tr>
 										<?php 
 									}
