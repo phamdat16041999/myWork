1873c982ed699b0b94fa02760ff30c2be2603583
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-16 10:29:20 +0700

    Update duantrienkhai.php

--- admin/duantrienkhai.php
+++ admin/duantrienkhai.php
@@ -13,6 +13,8 @@
 
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<style>
 		.td{
 			width: 20%;
