c984e68a94f1a5b9838907888e25437e4ddf4eb5
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-16 09:01:21 +0700

    a

--- admin/admin.php
+++ admin/admin.php
@@ -61,70 +61,27 @@
 								?>
 							</div>
 							<div class="col-lg-12 col-md-12" style="margin-top: 20px; background-color: #F8FAD5; ">
-								<div>
-									<h3 style="text-align: center; width: 100%">Thư ngỏ</h3>
-									<div style="text-indent: 20px"><em style="font-weight: 600">Kính gửi: </em>Quý Doanh nghiệp</div>
-									<div style="text-indent: 20px">
-										Thay mặt Ban Lãnh đạo cùng toàn thể đội ngũ Cán bộ, nhân viên <span style="font-weight: 600">Công ty Cổ phần HANAGO,</span> xin gửi tới Quý Doanh nghiệp lời chào trân trọng và thịnh vượng. Chúc cho sự hợp tác đôi bên bền vững, thành công.								
-									</div>
-									<br>
-									<div style="text-indent: 20px">
-										<span style="font-weight: 600">Công ty Cổ phần HANAGO</span> ra đời với sứ mệnh cung cấp các giải pháp tổng thể hỗ trợ và đồng hành cùng doanh nghiệp góp phần xây dựng, phát triển nển kinh tế trong giai đoạn Việt nam hội nhập kinh tế quốc tế. Đặc biệt là kể từ năm 2016, Nhà nước đưa việc phát triển và nâng tầm doanh nghiệp Việt trở thành một trong những mục tiêu trọng điểm, từ đó mở ra hành lang pháp lý và cơ hội cho các doanh nghiệp.
+								<img src="img/admin.png" alt="" style="width: 100%; height: auto;">
+							</div>	
 
-									</div>
-									<br>
-									<div style="text-indent: 20px">
-										Với tầm nhìn dài hạn và quan điểm phát triển bền vững, <span style="font-weight: 600">TESA</span> Hướng tới trở thành đơn vị dẫn đầu thị trường về dịch vụ tư vấn tổng thể doanh nghiệp và trực tiếp triển khai những dịch vụ tư vấn tổng thể doanh nghiệp và trực tiếp triển khai những dịch vụ đã tư vấn sao cho phù hợp với tình hình phát triển của doanh nghiệp
+						</div>
 
-									</div>
-									<br>
-									<div style="text-indent: 20px">
-										Với tôn chỉ <span style="font-weight: 600">“Chuyên nghiệp – Trách nhiệm – Sáng tạo”</span> cùng Ban Lãnh đạo và đội ngũ chuyên gia có nhiều năm kinh nghiệm tư vấn, quản lý, điều hành, chúng tôi luôn sẵn sàng đáp ứng tối đa mọi yêu cầu nhằm đưa ra những giải pháp tổng thể giúp tối ưu hóa hoạt động của Quý Doanh nghiệp
 
-									</div>
-									<br>
-									<div style="text-indent: 20px">
-										Đồng hành cùng sự thành công của Quý Doanh nghiệp là niềm tự hào của chúng tôi. Với khát vọng <span style="font-weight: 600">“Nâng tầm doanh nghiệp Việt”</span>, TESA rất hy vọng có cơ hội hợp tác và góp phần vào sự phát triển thịnh vượng của quý doanh nghiệp trong xu thế hội nhập toàn cầu
-
-									</div>
-									<br>
-									<div>
-										Trân trọng!
-									</div>
-									<br>
-									<div style="float: left; width: 60%; height: 200px;">
-
-									</div>
-									<div style="float: left; width: 40%; text-align: center;">
-										<H5>Tổng giám đốc</H5>
-										<br>
-										<br>
-										<br>
-										<br>
-										<h5>Nguyễn Văn Hưng</h5>
-									</div>
-								</div>
-							</div>
-						</div>	
 
 					</div>
-
-
-
+					<!-- phan lien he  -->
+					<?php 
+					include 'enpage.php';
+					?>
 				</div>
-				<!-- phan lien he  -->
-				<?php 
-				include 'enpage.php';
-				?>
-			</div>
-			<?php
+				<?php
+			}
+			else
+			{
+				echo 'Not found 404';
+			}
 		}
-		else
-		{
-			echo 'Not found 404';
-		}
-	}
-	?>
+		?>
 
-</body>
-</html>
+	</body>
+	</html>
