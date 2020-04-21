f49ca4d2aee954e64853193df5fe79a0fcae98ae
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-04-21 07:25:40 +0700

    test

---
+++ admin/select account.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<style>
+		td{
+			width: 7%;
+			height: auto;
+		}
+		.tablelienhe{
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
+		.trlienhe{
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
+		.tdlienhe{
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
+	</style>
+</head>
+<body>
+	<?php include 'connect.php' ?>
+	<?php 
+	if(isset($_GET['username'])&&isset($_GET['password']))
+	{
+		$a=$_GET['username'];
+		$b=$_GET['password'];
+		$query = "SELECT username, password FROM account WHERE username = '" . $a . "' and password = '". $b ."'" ;
+		$stmt = $pdo->prepare($query);
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
+
+		$username = $resultSet[0]["username"];
+		$password = $resultSet[0]["password"];
+
+
+		if ((count($resultSet)>0))
+		{
+			?>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<a href="admin.php?username=<?=$username?>&password=<?=$password?>" title=""><img src="img/Logo.png" alt="" style="width: 100%; height: auto;"></a>
+					</div>
+					<div class="col-sm-10" >
+						<h2 style="text-align: center; width: 80%; font-weight: 500;">CÔNG TY CỔ PHẦN HANAGO QUỐC TẾ</h2>
+					</div>
+				</div>
+			</div>
+
+
+
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-9 col-md-9" style="background-color: #CC681D; width: 100%; height: auto;">
+						<div style="width: 100%">
+							<?php 
+							include 'menuTop.php';
+							?>
+						</div>
+					</div>
+					<div style="background-color: #F8FAD5; width: 100%; height: auto;">
+						<form action="" method="POST" accept-charset="utf-8">
+							Nhập ngày cần tìm: <input type="text" name="searchlienhe" placeholder="yyyy/mm/dd">
+							<input type="submit" name="" value="Tìm kiếm">
+						</form>
+					</div>
+					<div style="background-color: #F8FAD5; width: 100%; height: auto;">
+						<table class="tablelienhe">
+							<tr class="trlienhe">
+								<td class="tdlienhe">Họ và tên</td>
+								<td class="tdlienhe">Địa chỉ</td>
+								<td class="tdlienhe">Điện thoại</td>
+								<td class="tdlienhe">Gmail</td>
+								<td class="tdlienhe">Mục đích gửi</td>
+								<td class="tdlienhe">Tiêu đề</td>
+								<td class="tdlienhe">Nội dung</td>
+								<td class="tdlienhe">Ngày liên hệ</td>
+							</tr>
+							<?php 
+							$sql = "select * from account";
+							$stmt = $pdo->prepare($sql);
+							$stmt->setFetchMode(PDO::FETCH_ASSOC);
+							$stmt->execute();
+							$resultSet = $stmt->fetchAll();
+							for($i=0; $i<count($resultSet); $i++)
+							{
+								?>
+								<tr class="trlienhe">
+									<td class="tdlienhe"><?=$resultSet[$i]["id"]?></td>
+									<td class="tdlienhe"><?=$resultSet[$i]["username"]?></td>
+									<td class="tdlienhe"><?=$resultSet[$i]["password"]?></td>
+
+								</tr>
+								<?php 
+							}
+							?>
+
+						</table>
+					</div>
+					<?php 
+					include 'enpage.php';
+					?>
+				</div>
+			</div>
+			<!-- phan lien he  -->
+
+			<?php
+		}
+		else
+		{
+			echo 'Not found 404';
+		}
+	}
+	?>
+
+</body>
+</html>
---
+++ admin/updateaccount.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<?php include 'connect.php' ?>
+	<?php 
+	if(isset($_GET['username'])&&isset($_GET['password']))
+	{
+		$a=$_GET['username'];
+		$b=$_GET['password'];
+		$query = "SELECT username, password FROM account WHERE username = '" . $a . "' and password = '". $b ."'" ;
+		$stmt = $pdo->prepare($query);
+		$stmt->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt->execute();
+		$resultSet = $stmt->fetchAll();
+
+		$username = $resultSet[0]["username"];
+		$password = $resultSet[0]["password"];
+
+
+		if ((count($resultSet)>0))
+		{
+			?>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-2">
+						<a href="admin.php?username=<?=$username?>&password=<?=$password?>" title=""><img src="img/Logo.png" alt="" style="width: 100%; height: auto;"></a>
+					</div>
+					<div class="col-sm-10" >
+						<h2 style="text-align: center; width: 80%; font-weight: 500;">CÔNG TY CỔ PHẦN HANAGO QUỐC TẾ</h2>
+					</div>
+				</div>
+			</div>
+
+
+
+			<div class="container">
+				<div class="row" style=" border-radius: 30px; border-style: solid;">
+					<?php 
+					include 'menuLeft.php';
+					?>
+
+					<div class="col-lg-9 col-md-9" style="background-color: #CC681D; border-top-right-radius: 20px; border-bottom-right-radius: 20px;">
+						<div class="row">
+							<div class="col-lg-12 col-md-12">
+								<?php 
+								include 'menuTop.php';
+								?>
+							</div>
+							<div class="col-lg-12 col-md-12" style="margin-top: 20px; background-color: #F8FAD5; text-align: center; margin-bottom: 20px">
+								<div style="text-align: center;">
+									<H5>Cập nhật dự án</H5>
+								</div>
+								<?php 
+								if(isset($_GET['id']));
+								$id=$_GET['id'];
+								$query = "select * from duantrienkhai where id = '" .$id."'";
+								$stmt = $pdo->prepare($query);
+								$stmt->setFetchMode(PDO::FETCH_ASSOC);
+								$stmt->execute();
+								$resultSet = $stmt->fetchAll();
+								for($i=0; $i<count($resultSet); $i++)
+								{
+									?>
+									<table style="width:100%">
+										<form action="" method="POST" accept-charset="utf-8">
+											<tr>
+												<td><div class="textlienhe">ID :</div></td>
+												<td><input type="text" name="id" style="width: 80%" value="<?=$resultSet[$i]["id"]?>"></td>
+											</tr>
+											<tr>
+												<td><div class="textlienhe">Địa chỉ :</div></td>
+												<td><input type="text" name="diachi" style="width: 80%" value="<?=$resultSet[$i]["diachi"]?>"></td>
+											</tr>
+											<tr>
+												<td><div class="textlienhe">Tên dự án :</div></td>
+												<td><input type="text" name="tenduan" style="width: 80%" value="<?=$resultSet[$i]["tenduan"]?>"></td>
+											</tr>
+											<tr>
+												<td><div class="textlienhe">Đường dẫn ảnh :</div></td>
+												<td><textarea name="duongdananh" style="width: 80%; overflow: auto;height: 200px"><?=$resultSet[$i]["duongdananh"]?></textarea></td>
+											</tr>
+											<tr>
+												<td colspan="2" style="text-align: center;"><input type="submit" name="" value="sửa dự án"></td>
+											</tr>
+										</form>
+									</table>
+									<?php 
+								}
+								?>
+
+								<?php 
+								if(isset($_POST['id'])&&isset($_POST['diachi'])&&isset($_POST['tenduan'])&&isset($_POST['duongdananh']))
+								{
+
+									$id=$_POST['id'];
+									$diachi=$_POST['diachi'];
+									$tenduan=$_POST['tenduan'];
+									$duongdananh=$_POST['duongdananh'];
+									$update = "Update duantrienkhai Set id = '" .$id."', diachi = '" .$diachi."', tenduan = '" .$tenduan."', duongdananh = '" .$duongdananh."' where id ='" . $id."'";
+									$stmt = $pdo->prepare($update);	
+									$stmt->execute();
+									if($stmt != null){
+										?>
+										<script>
+
+											setTimeout(function()
+											{ 
+												window.location = "duantrienkhai.php?username=<?=$username?>&password=<?=$password?>"; 
+											}, 10);
+
+										</script>
+										<?php
+										echo "Product has update successfully!";
+									}
+									else
+										echo "Try again";
+								}
+								?>
+
+
+
+
+
+
+
+							</div>	
+
+						</div>
+
+
+
+					</div>
+				</div>
+				<!-- phan lien he  -->
+				<?php 
+				include 'enpage.php';
+				?>
+
+				<?php
+			}
+			else
+			{
+				echo 'Not found 404';
+			}
+		}
+		?>
+
+	</body>
+	</html>
