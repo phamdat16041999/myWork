f4b29ab942d5fa387000688b2d8095e35a0f32e7
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-05-02 18:24:16 +0700

    aaaaa

--- menuLeft.php
+++ menuLeft.php
@@ -8,7 +8,10 @@
 				<div class="row" style="margin-top: 40px">
 					<div class="col-lg-12 col-md-12">
 						<div class="sidenav" style="line-height: 1.15;">
-							<button class="dropdown-btn" id="btnService"><h3 class="texttitle">Dịch vụ</h3>
+							<button class="dropdown-btn" id="btnService">
+								<div style="width: 100%; margin-left: 5%;">
+									<h3 class="texttitle">Dịch vụ</h3>
+								</div>
 								<i class="fa fa-caret-down"></i>
 							</button>
 							<div class="dropdown-container">
