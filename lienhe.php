1eed10353e525b9aee4b0ecbf7889ad44bd2e257
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-05-03 17:21:18 +0700

    Update lienhe.php

--- lienhe.php
+++ lienhe.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="/style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
 	<!-- jQuery library -->
