d05c9559368998bb390820611a5d1e39f355bdfa
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-05-03 18:23:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -53,15 +53,21 @@
 					<div class="slideshow-container">
 
 						<div class="mySlides fade">
+							<div class="numbertext">1 / 3</div>
 							<img src="img/2.jpg" class="imghome">
+							<div class="text">Caption Text</div>
 						</div>
 
 						<div class="mySlides fade">
+							<div class="numbertext">2 / 3</div>
 							<img src="img/3.jpg" class="imghome">
+							<div class="text">Caption Two</div>
 						</div>
 
 						<div class="mySlides fade">
+							<div class="numbertext">3 / 3</div>
 							<img src="img/4.jpg" class="imghome">
+							<div class="text">Caption Three</div>
 						</div>
 
 					</div>
