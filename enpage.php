be38c32ace782c32a92ae189a64dab7f316d5a9a
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2020-05-03 19:10:17 +0700

    fix endpage

--- enpage.php
+++ enpage.php
@@ -14,7 +14,8 @@
 							<td align="left" valign="top">
 								<p style="font-size:12px">
 									<strong>Copyright © 2011: </strong><strong>Công ty cổ phần HANAGO quốc tế </strong>- Hotline : <strong>0968. 353.436</strong><br>
-									<strong>Trụ sở chính: </strong>Số 5 Ngõ 10, Phố Võng Thị, Phường Bưởi, Q. Tây Hồ, TP.Hà Nội<br>
+									<strong>Địa chỉ: </strong>Ô111, Lô A, KĐT Đại Kim - Định Công, Phường Định Công, Quận Hoàng Mai, TP. Hà Nội<br>
+									<strong>Tel</strong>: 02462.594.592 <strong style="margin-left: 20px">Fax</strong>: 0243.6413.412 <br>
 									<strong>Email: </strong>Hanago2526@gmail.com
 								</p>
 							</td>
